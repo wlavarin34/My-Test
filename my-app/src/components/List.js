fbef0d73ac00ba11e8e8c27557a7ca5a5db196b6
Wilkens Lavarin <wilkenslavarin@aol.com>
2022-02-18 14:39:53 -0500

    Update List.js

--- my-app/src/components/List.js
+++ my-app/src/components/List.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const List = ({list}) => {
   return (
-    <div><ul style={{'padding':'30px'}} className="list-group">
+    <div><ul style={{padding:'30px'}} className="list-group">
     {list.map((i,idx,array)=>(  <li key={idx} className="list-group-item">Word: {i.word} <br></br> Count: {i.count}</li>
 ))}
   </ul></div>
